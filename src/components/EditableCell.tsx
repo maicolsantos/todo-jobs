04c753d7c454b68bb7ea603aa808dec43c7d623e
Maicol Santos <maicolcostaa8@gmail.com>
2024-05-09 00:15:11 +0100

    Remove not used code

--- src/components/EditableCell.tsx
+++ src/components/EditableCell.tsx
@@ -1,4 +1,4 @@
-import { Input, Typography } from "antd";
+import { Input } from "antd";
 import { useState } from "react";
 import { Jobs } from "../@types/jobs";
 import { useJobs } from "../store/useJobs";
@@ -9,8 +9,6 @@ type EditableCellProps = {
   currentItem: Jobs;
   children?: React.ReactNode;
 };
-
-const { Text } = Typography;
 
 export const EditableCell = ({
   dataKey,
